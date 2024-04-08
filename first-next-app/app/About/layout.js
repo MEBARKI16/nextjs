491eb6292440f44786a0eb0f490971eb61c3bded
Amine MEBARKI <105977621+MEBARKI16@users.noreply.github.com>
2024-04-08 12:21:13 +0100

    working with layout.js

---
+++ first-next-app/app/About/layout.js
@@ -0,0 +1,17 @@
+
+
+export const metadata = {
+    title: 'hello',
+    description: 'hello2',
+};
+
+export default function layout({ children }) {
+    return (
+       <div>
+        <h1>hello</h1>
+         {children}
+       </div>
+          
+        
+    );
+}

7bc54b95dad4ce899d24d88684571e55f2e73d86
Amine MEBARKI <105977621+MEBARKI16@users.noreply.github.com>
2024-04-09 11:59:34 +0100

    Revisiting The Concept Of Layouts

---
+++ foodies/app/meals/layout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const layout = ({ children }) => {
+    return (
+        <>
+            <p>Meals Layout</p>
+            {children}
+        </>
+    )
+}
+
+export default layout

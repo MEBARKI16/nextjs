05e573628935e7426355a16a6e0c9581ddd0b715
Amine MEBARKI <105977621+MEBARKI16@users.noreply.github.com>
2024-04-14 17:06:19 +0100

    update

--- foodies/app/meals/page.js
+++ foodies/app/meals/page.js
@@ -1,13 +1,16 @@
 import React from 'react'
+import classes from "./page.module.css"
 
 const PageMeals = () => {
     console.log("meals")
     return (
-        <main>
-            <h1 style={{ color: 'white', textAlign: 'center' }}>
-                Page Meals
-            </h1>
-        </main>
+        <>
+            <header className={classes.header}>
+                <h1>Des repas délicieux, créés <span className={classes.highlight}>par vous</span></h1>
+                <p>Choisissez votre recette préférée et cuisinez-la vous-même. c'est facile et amusant</p>
+            </header>
+            <main className={classes.main}></main>
+        </>
     )
 }
 

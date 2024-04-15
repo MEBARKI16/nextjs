7352d6344793bdd2074677740c5706f8cb431e2b
Amine MEBARKI <105977621+MEBARKI16@users.noreply.github.com>
2024-04-15 11:18:52 +0100

    Outputting Meals Data & Images With Unknown Dimensions

--- foodies/app/meals/page.js
+++ foodies/app/meals/page.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import classes from "./page.module.css"
+import Link from 'next/link'
+import MealsGrid from '@/components/Meals/MealsGrid'
 
 const PageMeals = () => {
     console.log("meals")
@@ -8,8 +10,15 @@ const PageMeals = () => {
             <header className={classes.header}>
                 <h1>Des repas délicieux, créés <span className={classes.highlight}>par vous</span></h1>
                 <p>Choisissez votre recette préférée et cuisinez-la vous-même. c'est facile et amusant</p>
+                <p className={classes.cta}>
+                    <Link href="/meals/share">
+                        Partagez Votre Recette Préférée
+                    </Link>
+                </p>
             </header>
-            <main className={classes.main}></main>
+            <main className={classes.main}>
+                <MealsGrid meals={[]} />
+            </main>
         </>
     )
 }

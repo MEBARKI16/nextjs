84fcbd0b80527c1c3c55207cf85cc6e1b23e8c25
Amine MEBARKI <105977621+MEBARKI16@users.noreply.github.com>
2024-04-21 14:28:06 +0100

    Adding A Loading Page

--- foodies/app/meals/page.js
+++ foodies/app/meals/page.js
@@ -1,15 +1,16 @@
-import React from 'react'
-import classes from "./page.module.css"
-import Link from 'next/link'
-import MealsGrid from '@/components/Meals/MealsGrid'
+import React from 'react';
+import classes from "./page.module.css";
+import Link from 'next/link';
+import MealsGrid from '@/components/Meals/MealsGrid';
+import { GetMeals } from '@/lib/meals';
 
-const PageMeals = () => {
-    console.log("meals")
+async function PageMeals() {
+    const meals = await  GetMeals();
     return (
         <>
             <header className={classes.header}>
                 <h1>Des repas délicieux, créés <span className={classes.highlight}>par vous</span></h1>
-                <p>Choisissez votre recette préférée et cuisinez-la vous-même. c'est facile et amusant</p>
+                <p>Choisissez votre recette préférée et cuisinez-la vous-même. C'est facile et amusant!</p>
                 <p className={classes.cta}>
                     <Link href="/meals/share">
                         Partagez Votre Recette Préférée
@@ -17,10 +18,10 @@ const PageMeals = () => {
                 </p>
             </header>
             <main className={classes.main}>
-                <MealsGrid meals={[]} />
+                <MealsGrid meals={meals} />
             </main>
         </>
-    )
+    );
 }
 
-export default PageMeals
+export default PageMeals;

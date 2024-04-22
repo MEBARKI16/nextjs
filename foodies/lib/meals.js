adaf79df6c4627ed3c52cfdf309bc0e18cfdfeb1
Amine MEBARKI <105977621+MEBARKI16@users.noreply.github.com>
2024-04-22 12:05:54 +0100

    Handling Errors

--- foodies/lib/meals.js
+++ foodies/lib/meals.js
@@ -4,5 +4,6 @@ const db = sql("meals.db");
 
 export async function GetMeals() {
     await new Promise((resolve) => setTimeout(resolve, 5000))
+    throw new Error('Erreur lors de la récupération des données');
     return db.prepare("SELECT * FROM meals").all()
 }
